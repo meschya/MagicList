45159de35e87ee8d5f747e64ddebbc720da5e525
meschya <meschyaaa@gmail.com>
2022-04-22 18:49:59 +0300

    add custom color

---
+++ MagicList/Extension/Color+.swift
@@ -0,0 +1,16 @@
+import Foundation
+import SwiftUI
+import UIKit
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+// Example: Color.theme.secondaryText
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let title = Color("TitleColor")
+    let secondTitle = Color("SecondTitleColor")
+    let welcomeGray = Color("WelcomeColor")
+}
---
+++ MagicList/Extension/UIColor+.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+extension UIColor {
+    static let theme = CustomColor()
+}
+
+// Example: UIColor.theme.accent
+struct CustomColor {
+    let accent = UIColor(named: "AccentColor")
+    let background = UIColor(named:"BackgroundColor")
+    let title = UIColor(named:"TitleColor")
+    let secondTitle = UIColor(named:"SecondTitleColor")
+    let welcomeGray = UIColor(named:"WelcomeColor")
+}
