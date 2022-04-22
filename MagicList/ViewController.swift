1a89c3c0856d5cdc9a580c7e67ddc5f2889ef709
meschya <meschyaaa@gmail.com>
2022-04-22 14:05:40 +0300

    Initial Commit

---
+++ MagicList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicList
+//
+//  Created by Egor Mesheryakov on 22.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
